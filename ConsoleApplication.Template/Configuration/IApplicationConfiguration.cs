87db2009c07d97b4f511eb80ab34eba8d4d6258b
Leandro Viana <leandro.viana@xpand-it.com>
2022-05-15 01:17:12 +0100

    ProjectStruct

---
+++ ConsoleApplication.Template/Configuration/IApplicationConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication.Template.Configuration
+{
+    public class ApplicationConfiguration
+    {
+        public string Key { get; set; }
+    }
+}
---
+++ ConsoleApplication.Template/Program.cs
@@ -0,0 +1,52 @@
+﻿using ConsoleApplication.Template.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleApplication.Template
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var host = CreateHostBuilder(args).Build();
+            var configuration = host.Services.GetService<ApplicationConfiguration>();
+            var logger = host.Services.GetService<ILogger<Program>>();
+            host.Run();
+        }
+
+        /// <summary>
+        /// Inject Dependecies and add application.json configuration
+        /// </summary>
+        /// <param name="args">args from console input</param>
+        /// <returns></returns>
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            //Configuration
+            var builder = new ConfigurationBuilder()
+               .AddJsonFile($"appsettings.json", true, true);
+
+            var config = builder.Build();
+            var appconfiguration = config.Get<ApplicationConfiguration>();
+
+            //Create Dependicies
+            var hostBuilder = Host.CreateDefaultBuilder(args)
+                    .ConfigureServices((hostContext, services) =>
+                    {
+                        
+                        services.AddSingleton(appconfiguration);
+
+                        //Logging
+                        services.AddLogging();
+                        var provider = services.BuildServiceProvider();
+                        var factory = provider.GetService<ILoggerFactory>();
+                    });
+          
+
+            return hostBuilder;
+        }
+               
+    }
+}
